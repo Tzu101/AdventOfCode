5a123700db305e7cab9f5d5388454e4d0686a1b3
Tzu101 <val.waldner5@gmail.com>
2024-12-02 13:35:26 +0100

    Simplified line reading utility function

--- aoc/src/lib.rs
+++ aoc/src/lib.rs
@@ -3,17 +3,10 @@ use std::io::{BufRead, BufReader};
 use std::time::Instant;
 
 pub fn to_lines(file_path: &str) -> Vec<String> {
-    let file = File::open(file_path);
-    match file {
-        Ok(file) => {
-            let reader = BufReader::new(file);
-            let lines: Vec<String> = reader.lines().collect::<Result<_, _>>().unwrap();
-            lines
-        }
-        Err(err) => {
-            panic!("{}", err);
-        }
-    }
+    let file = File::open(file_path).unwrap();
+    let reader = BufReader::new(file);
+    let lines: Vec<String> = reader.lines().collect::<Result<_, _>>().unwrap();
+    lines
 }
 
 pub fn benchmark<F, R>(func: F) -> R
