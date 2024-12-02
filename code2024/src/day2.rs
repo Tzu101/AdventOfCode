751e2025d7c8bf35baec25c3f298c25f1ec675bb
Tzu101 <val.waldner5@gmail.com>
2024-12-02 10:38:23 +0100

    Improved Day 2 part 2 :)

--- code2024/src/day2.rs
+++ code2024/src/day2.rs
@@ -1,8 +1,78 @@
 const MIN_ORDER_DIFF: i32 = 1;
 const MAX_ORDER_DIFF: i32 = 3;
-fn is_ordered(prev: i32, next: i32) -> bool {
+fn are_levels_ordered(prev: i32, next: i32) -> bool {
     let diff: i32 = (next - prev) as i32;
     MIN_ORDER_DIFF <= diff && diff <= MAX_ORDER_DIFF
+}
+
+fn is_report_ordered(levels: &Vec<i32>) -> bool {
+    let mut is_increasing = true;
+    let mut is_decreasing = true;
+    let mut last_level = levels[0];
+    for l in 1..levels.len() {
+        is_increasing = is_increasing && are_levels_ordered(last_level, levels[l]);
+        is_decreasing = is_decreasing && are_levels_ordered(levels[l], last_level);
+
+        last_level = levels[l];
+    }
+
+    is_increasing || is_decreasing
+}
+
+fn is_report_almost_ordered(levels: &Vec<i32>) -> bool {
+    let mut is_increasing = true;
+    let mut last_level = levels[0];
+    for l in 1..levels.len() {
+        is_increasing = is_increasing && are_levels_ordered(last_level, levels[l]);
+
+        if is_increasing {
+            last_level = levels[l];
+        }
+        else {
+            let mut potential_solution1 = levels.clone();
+            potential_solution1.remove(l-1);
+            let potential_solved1 = is_report_ordered(&potential_solution1);
+
+            let mut potential_solution2 = levels.clone();
+            potential_solution2.remove(l);
+            let potential_solved2 = is_report_ordered(&potential_solution2);
+
+            if potential_solved1 || potential_solved2 {
+                return true;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    let mut is_decreasing = true;
+    let mut last_level = levels[0];
+    for l in 1..levels.len() {
+        is_decreasing = is_decreasing && are_levels_ordered(levels[l], last_level);
+
+        if is_decreasing {
+            last_level = levels[l];
+        }
+        else {
+            let mut potential_solution1 = levels.clone();
+            potential_solution1.remove(l-1);
+            let potential_solved1 = is_report_ordered(&potential_solution1);
+
+            let mut potential_solution2 = levels.clone();
+            potential_solution2.remove(l);
+            let potential_solved2 = is_report_ordered(&potential_solution2);
+
+            if potential_solved1 || potential_solved2 {
+                return true;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    is_increasing || is_decreasing
 }
 
 #[allow(dead_code)]
@@ -13,18 +83,7 @@ pub fn part1() -> String {
     for report in reports {
         let levels = report.split_whitespace().collect::<Vec<&str>>();
         let levels = levels.iter().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-
-        let mut is_increasing = true;
-        let mut is_decreasing = true;
-        let mut last_level = levels[0];
-        for l in 1..levels.len() {
-            is_increasing = is_increasing && is_ordered(last_level, levels[l]);
-            is_decreasing = is_decreasing && is_ordered(levels[l], last_level);
-
-            last_level = levels[l];
-        }
-
-        if is_increasing || is_decreasing {
+        if is_report_ordered(&levels) {
             safe_reports += 1;
         }
     }
@@ -40,25 +99,9 @@ pub fn part2() -> String {
     for report in reports {
         let levels = report.split_whitespace().collect::<Vec<&str>>();
         let levels = levels.iter().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-
-        let mut is_increasing = true;
-        let mut last_level = levels[0];
-        for l in 1..levels.len() {
-            is_increasing = is_increasing && is_ordered(last_level, levels[l]);
-
-            last_level = levels[l];
-        }
-
-        let mut is_decreasing = true;
-        let mut last_level = levels[0];
-        for l in 1..levels.len() {
-            is_decreasing = is_decreasing && is_ordered(levels[l], last_level);
-
-            last_level = levels[l];
-        }
-
-        if is_increasing || is_decreasing {
+        if is_report_almost_ordered(&levels) {
             safe_reports += 1;
+            println!("{:?}", report);
         }
     }
 
