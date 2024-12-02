2b42047f760b40db9add9a04f0d14ae908287680
Tzu101 <val.waldner5@gmail.com>
2024-12-02 09:22:42 +0100

    Solved Day 2 Part 1

--- code2024/src/day2.rs
+++ code2024/src/day2.rs
@@ -1,9 +1,73 @@
 #[allow(dead_code)]
 pub fn part1() -> String {
-    String::from("Part 1")
+    let mut safe_reports = 0u32;
+
+    let reports = aoc::to_lines("input/day2.txt");
+    for report in reports {
+        let levels = report.split_whitespace().collect::<Vec<&str>>();
+        let levels = levels.iter().map(|s| s.parse::<u32>().unwrap()).collect::<Vec<u32>>();
+
+        let mut is_increasing = true;
+        let mut is_decreasing = true;
+        let mut last_level = levels[0];
+        let mut allow_bad_level = true;
+        for l in 1..levels.len() {
+            if is_increasing {
+                if !(levels[l] > last_level && levels[l].abs_diff(last_level) <= 3) {
+                    is_increasing = false;
+                }
+            }
+
+            if is_decreasing {
+                if !(levels[l] < last_level && levels[l].abs_diff(last_level) <= 3) {
+                    is_decreasing = false;
+                }
+            }
+
+            last_level = levels[l];
+        }
+
+        if is_increasing || is_decreasing {
+            safe_reports += 1;
+        }
+    }
+
+    safe_reports.to_string()
 }
 
 #[allow(dead_code)]
 pub fn part2() -> String {
-    String::from("Part 2")
+    let mut safe_reports = 0u32;
+
+    let reports = aoc::to_lines("input/day2_example.txt");
+    for report in reports {
+        let levels = report.split_whitespace().collect::<Vec<&str>>();
+        let levels = levels.iter().map(|s| s.parse::<u32>().unwrap()).collect::<Vec<u32>>();
+
+        let mut is_increasing = true;
+        let mut is_decreasing = true;
+        let mut last_level = levels[0];
+        let mut allow_bad_level = true;
+        for l in 1..levels.len() {
+            if is_increasing {
+                if !(levels[l] > last_level && levels[l].abs_diff(last_level) <= 3) {
+                    is_increasing = false;
+                }
+            }
+
+            if is_decreasing {
+                if !(levels[l] < last_level && levels[l].abs_diff(last_level) <= 3) {
+                    is_decreasing = false;
+                }
+            }
+
+            last_level = levels[l];
+        }
+
+        if is_increasing || is_decreasing {
+            safe_reports += 1;
+        }
+    }
+
+    safe_reports.to_string()
 }
--- code2024/src/main.rs
+++ code2024/src/main.rs
@@ -7,5 +7,6 @@ fn main() {
     //println!("Result of day 1, part 2: {}", aoc::benchmark(day1::part2));
 
     // Day 2
-    println!("Result of day 2, part 1: {}", aoc::benchmark(day2::part1));
+    //println!("Result of day 2, part 1: {}", aoc::benchmark(day2::part1));
+    println!("Result of day 2, part 2: {}", aoc::benchmark(day2::part2));
 }
