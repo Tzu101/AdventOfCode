209c1948c8f2d5873a30109138ff8341d939f10b
Tzu101 <val.waldner5@gmail.com>
2024-12-03 08:49:36 +0100

    Day 3 Part 2 Solved

--- code2024/src/main.rs
+++ code2024/src/main.rs
@@ -12,6 +12,6 @@ fn main() {
     //println!("Result of day 2, part 2: {}", aoc::benchmark(day2::part2));
 
     // Day 3
-    println!("Result of day 3, part 1: {}", aoc::benchmark(day3::part1));
+    //println!("Result of day 3, part 1: {}", aoc::benchmark(day3::part1));
     println!("Result of day 3, part 2: {}", aoc::benchmark(day3::part2));
 }
